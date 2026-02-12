7b680411398f06fc3e148af8b15ce5d7e1209710
Arafat <arafat1234516@gmail.com>
2026-02-12 16:42:27 +0500

    Explore column values

--- eda.R
+++ eda.R
@@ -2,4 +2,12 @@
 data_path <- here::here("final_dataset", "train.csv")
 data <- data.table::fread(data_path)
 
-# 
+# look into columns
+colnames(data)
+drive_predictors <- colnames(data)[32:159]
+data[, unique(bonus_malus)]
+data[, unique(age_experience_name)]
+
+# crash rate by drivers age and experience
+data[, mean(is_claim), by = .(age_experience_name)]
+data[is_claim==1, unique(claim_cnt)]
