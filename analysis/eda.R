6a2e41354ed5a349c6bf75ee420baafbd3439f17
Arafat <arafat1234516@gmail.com>
2026-02-19 16:00:10 +0500

    Remove unique from eda. Move summary eda.R -> summary.md

--- analysis/eda.R
+++ analysis/eda.R
@@ -8,33 +8,17 @@ drive_predictors <- colnames(data)[32:159]
 
 # Null value percents per column
 typeof(colMeans(is.na(data)) * 100)
-# claim_amount and claim_cnt has null values for people who didn't made a claim
-# 11.5% of ownerkato values is missing
-# 19.4% percent of engine_volume and engine_power is missing
-# All of the driver predictor score columns has a missing value
 
-# basic stats for business
+# statistic for claims
 data[, mean(is_claim)]
 data[is_claim == 1, mean(claim_amount)]
 data[is_claim == 1, mean(claim_cnt)]
-data[, mean(premium)]
 data[is_claim == 1, sum(claim_amount)]/data[, sum(premium_wo_term)]*100
-# 2 percent end up in crashes. Company pays 668735.8 per claim.
-# On average company pays 13026.95 to the insured.
-# On average company receives 10554.13 from premiums (taking into acount cancelations)
-# Thus company is losing money at the current period
-# The ratio of payments to premuims is 123 percent. This puts company at the loss.
 
-# look into cancelation of insurance
+# premiums
+data[, mean(premium_wo_term)]
+data[, mean(premium)]
 data[premium==premium_wo_term, .N]/data[, .N]
-# 28.5 percent of client cancel their insurance
 
-# crash rate by drivers age and experience
-data[, mean(is_claim), by = .(age_experience_name)]
-
-# bonus malus types
-data[, unique(bonus_malus)]
-
-# is individual_person_name
-data[, unique(is_individual_person)]
-data[, unique(is_residence_name)]
+# profitability
+data[is_claim == 1, sum(claim_amount)]/data[, sum(premium_wo_term)]*100
--- analysis/summary.md
+++ analysis/summary.md
@@ -1,3 +1,12 @@
+#### Business stats
+* 2 percent end up in crashes. Company pays 668735 per claim. 
+Therefore, on average company pays 13030 to the insured.
+
+* On average company receives 10555 from premiums (taking into account cancellations). 
+Average premium is 13290 and 28.5% end up canceling their insurance.
+
+* Thus, company is losing money at the current period. The ratio of payments to premiums is 123%.
+
 #### Null Values
 * `claim_amount` and `claim_cnt` has null values for people who didn't made a claim
 * 11.5% of `ownerkato` values is missing
