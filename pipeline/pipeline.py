50c87e8b257b53f7426bdd8cf95a0311b9342016
Arafat <arafat1234516@gmail.com>
2026-02-20 11:25:21 +0500

    Fix typo, rename lazy_df -> lf

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -10,15 +10,15 @@ class Pipeline:
 
     def preprocess(self):
         # Scan csv file into lazy dataframe 
-        self.lazy_df = pl.scan_csv(FILEPATH, schema_overrides = SCHEMA_OVERRIDES)
+        self.lf = pl.scan_csv(FILEPATH, schema_overrides = SCHEMA_OVERRIDES)
         
         # Drop columns
-        self.lazy_df.drop(DROP_COLUMNS)
+        self.lf = self.lf.drop(DROP_COLUMNS)
 
     def _load(self):
         if self._train_df is None:
-            self.proprocess()
-            df = self.lazy_df.collect()
+            self.preprocess()
+            df = self.lf.collect()
             self._train_df = df.sample(fraction = self._train_fraction, seed = self._seed)
             self._test_df = df.join(self._train_df, how = "anti", on = df.columns)
 
